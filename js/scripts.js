90229d00faff69036f980af509b2d2be7fee8e0f
jaki widby and soha pourpirali <soha@>
2019-11-13 20:10:51 -0800

    add clickable buttons that change the background color of the body.

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,21 @@ $(document).ready(function() {
   $("p").click(function() {
     $(".word").slideToggle();
   });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+
 });
